154644b11c6f4f41373d568d64d3188cfe0d949a
JAVAVEEN <naveenbsc.mca1518@gmail.com>
2024-07-03 20:18:45 +0530

    about page design

--- firsttask/src/Components/About.js
+++ firsttask/src/Components/About.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import '../CSS/About.css'
+import introNaveen from '../Images/introductionNaveen.png'
 
 export default function About() {
+  const cardWidth = {
+    width:'18rem'
+  }
   return (
     <div>
-      About Page
+      <div className='container bg-dark aboutDiv'>
+        <h1>Self Introduction</h1>
+        <div className='container selfIntro'>
+          <div className='row'>
+            <div className='col'>
+              <div class="card" style={cardWidth}>
+                <img src="..." class="card-img-top" alt="..."/>
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="" class="btn btn-primary">Go somewhere</a>
+                  </div>
+              </div>
+            </div>
+            <div className='col'>
+              <h2>Naveen kumar.S</h2>
+              <p>Fullstack Developer</p>
+              <div>
+                <img src={introNaveen} alt='naveen Introduction' width={200} className='introImage' />
+              </div>
+            </div>
+            <div className='col'>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- firsttask/src/Components/Project.js
+++ firsttask/src/Components/Project.js
@@ -1,22 +1,54 @@
 import React from 'react'
 import '../CSS/Project.css'
 import trackerLogo from '../Images/locationTracker.png'
+import eCommerce from '../Images/ecommerce.jpg'
+import rentalECommerce from '../Images/rental_Ecommerce.jpg'
+
 export default function Project() {
   return (
     <div >
       <div className='container-fluid bg-dark projectsMainDiv'>
-        <div className='headerDiv bg-success container p-3'>
+        <div className='headerDiv container p-3'>
           <h1>Our Projects</h1>
         </div>
         <div className='container-fluid bg-light projectContainer'>
-          <div className='row'>
-            <div className='col'>
-              <img className='busProjectImg' src={trackerLogo} alt='area Tracer'/>
-            </div>
-            <div className='col busProjectText'>
-              <h1>Bus Tracker</h1>
+          <div className='projectImage'>
+            <img className='busProjectImg' src={trackerLogo} alt='area Tracer' />
+          </div>
+          <div className='busProjectText'>
+            <div>
+              <h1 className='projectText'>Bus Tracker</h1>
             </div>
           </div>
+        </div>
+        <div className='container bg-success p-3 descContainer'>
+          <h1>Through this project we can track The availability of buses for the current time, then can we view the driver & conductor details, and track our bus routes where it comes from and where it is go, and what time the bus reach your current location. </h1>
+        </div>
+        <div className='container-fluid bg-light projectContainer'>
+          <div className='projectImage'>
+            <img className='ecommerceImg' src={eCommerce} alt='area Tracer' />
+          </div>
+          <div className='busProjectText'>
+            <div>
+              <h1 className='ecommerceText'>E-Commerce</h1>
+            </div>
+          </div>
+        </div>
+        <div className='container bg-success p-3 descContainer'>
+          <h1>In this project we have implement the Rental based Forklift spares for sales we also implement the specification for the product and it's Prize we have also used filters based on specification & it's Rates after we have register our user Id for continue buisness with those peoples.</h1>
+        </div>
+        <div className='container-fluid bg-light projectContainer'>
+          <div className='projectImage'>
+            <img className='ecommerceImg' src={rentalECommerce} alt='area Tracer' />
+          </div>
+          <div className='busProjectText'>
+            <div>
+              <h1 className='ecommerceText'>Rental E-Commerce</h1>
+            </div>
+          </div>
+        </div>
+        <div className='container bg-success p-3 descContainer'>
+          <h1>In this project provides Rental based construction equipment we track the rental time using Javascript frontend it could be ReactJs, then Backend used Mongodb to store the details for Water cans, and then the additional future is expense tracker of water can supply.</h1>
         </div>
       </div>
     </div>
